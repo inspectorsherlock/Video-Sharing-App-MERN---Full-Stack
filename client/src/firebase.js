4853fbb91dc0801e68c00cb8aa1f4485df10fa60
Fawwaz <fawwaz.chowdhury@googlemail.com>
2022-10-06 17:16:10 +0100

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcvK8q2eXn6x071lEGy-piAu7RJTMZjeM",
-  authDomain: "video-e3901.firebaseapp.com",
-  projectId: "video-e3901",
-  storageBucket: "video-e3901.appspot.com",
-  messagingSenderId: "18051065281",
-  appId: "1:18051065281:web:21cb7ab8906bc528e9fb40"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
